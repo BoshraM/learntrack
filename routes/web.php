865100f000db7b3e6ed67cd492e12d13fbb3dd8c
Boshra Mahmoudi <boshra@Boshras-MBP-2.lan>
2026-03-19 02:32:56 +0000

    change the route of logo to welocome page

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\UserProgressController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
